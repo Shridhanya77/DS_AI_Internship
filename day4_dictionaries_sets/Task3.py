75cd6a22c1dda7d0fa0fd6cccf283c390c2dc896
Shridhanya <kulaldhanya35@gmail.com>
2026-02-06 13:08:36 +0400

    Delete day4_dictionaries_sets directory

--- day4_dictionaries_sets/Task3.py
+++
@@ -1,8 +0,0 @@
-friend_a={"Python","Cooking","Hiking","Movies"}
-friend_b={"Hiking","Gaming","Photography","Python"}
-common_interests=friend_a & friend_b
-all_interests=friend_a | friend_b
-unique_to_a=friend_a-friend_b
-print("Common interests :",common_interests)
-print("All interests : " ,all_interests)
-print("Interests only friend A has :" ,unique_to_a)
--- day4_dictionaries_sets/practice3.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter number of customers"))
-user_purchases={}
-for i in range(n):
-    name=input("Enter Customer name : ")
-    amount=int(input(f"Enter purchase amount for {name} :"))
-    user_purchases[name]=amount
-    print("Customer purchase data :" ,user_purchases)
-
