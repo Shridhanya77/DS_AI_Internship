937def5aee1641662cf11e1cfb414c595b93fb80
Shridhanya77 <kulaldhanya35@gmail.com>
2026-02-10 12:51:22 +0400

    Commit changes

--- day5_functions/practice4.py
+++
@@ -1,4 +0,0 @@
-import math
-import random
-print(math.sqrt(4))
-print(random.randint(1,10))
--- day7_file_handling/Task3.py
+++
@@ -1,8 +0,0 @@
-filename=input("Enter your filename : ")
-try:
-    with open(filename,"r") as file:
-        content=file.read()
-        print("File content : ")
-        print(content)
-except FileNotFoundError:
-    print("Oops! That file doesn't exist yet!")
